445aa28201da954763ee11cc8ef54459b60e062e
Diego Guerrero <zorro8815@gmail.com>
2016-03-20 10:41:39 -0430

    agregando barra de herramientas, con iconos y posicion

--- MainMenu.py
+++ MainMenu.py
@@ -1,6 +1,7 @@
 # .*. coding: utf-8
 
 from PyQt4.QtGui import *
+from PyQt4.QtCore import Qt
 import sys
 
 
@@ -17,8 +18,21 @@ class MainWindow(QMainWindow):
 
         #QAction
         self.accionSalir = QAction("Salir", self)
+        self.accionSalir.setIcon(QIcon("salir.png"))
         self.accionSalir.setShortcut("Ctrl+Q")
         self.accionSalir.setStatusTip(self.trUtf8("Salir de la aplicación"))
+
+
+        self.accionNuevo = QAction("Nuevo", self)
+        self.accionNuevo.setIcon(QIcon("nuevo.png"))
+        self.accionNuevo.setShortcut("Ctrl+N")
+        self.accionNuevo.setStatusTip(self.trUtf8("Nuevo"))
+
+        #Barra de herramientas o toolbar
+        self.toolbar = QToolBar(self)
+        self.toolbar.addAction(self.accionNuevo)
+        self.toolbar.addAction(self.accionSalir)
+        self.addToolBar(Qt.RightToolBarArea, self.toolbar)
 
         #Barra de estado
         self.setStatusBar(QStatusBar())
@@ -26,6 +40,7 @@ class MainWindow(QMainWindow):
         #Menu
         menu = self.menuBar()
         menu_archivo = menu.addMenu("&Archivo")
+        menu_archivo.addAction(self.accionNuevo)
         menu_archivo.addAction(self.accionSalir)
 
 
